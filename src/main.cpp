481492c5961585ed23067883ab1ec7e84022f762
Trevor Last <trevorclast@gmail.com>
2022-06-26 13:18:04 -0400

    Refactor SDL stuff into App class

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,112 @@
 #include <SDL.h>
 
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <unordered_map>
+
+
+/**
+ * The application.
+ */
+class App
+{
+private:
+    std::unordered_map<
+        Uint32,
+        std::vector<std::function<void(SDL_Event)>>> _event_callbacks;
+    SDL_Window *const _window;
+    SDL_GLContext const _context;
+public:
+    int window_width,
+        window_height;
+    bool running;
+
+    App(std::string title, int width, int height)
+    :   _event_callbacks{}
+    ,   _window{SDL_CreateWindow(
+            title.c_str(),
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            width, height,
+            SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE)}
+    ,   _context{SDL_GL_CreateContext(_window)}
+    ,   window_width{width}
+    ,   window_height{height}
+    ,   running{true}
+    {
+        glViewport(0, 0, window_width, window_height);
+    }
+
+    ~App()
+    {
+        SDL_GL_DeleteContext(_context);
+        SDL_DestroyWindow(_window);
+    }
+
+
+    /** Set up an event callback. */
+    void add_callback(
+        SDL_EventType event, std::function<void(SDL_Event)> callback)
+    {
+        _event_callbacks.insert(
+            {event, std::vector<std::function<void(SDL_Event)>>{}});
+        _event_callbacks[event].push_back(callback);
+    }
+
+
+    /** Handle input events. */
+    void input()
+    {
+        SDL_Event event{};
+        while (SDL_PollEvent(&event))
+        {
+            // Built-in actions.
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                running = false;
+                break;
+            case SDL_WINDOWEVENT:
+                if (event.window.event == SDL_WINDOWEVENT_RESIZED)
+                {
+                    // Viewport size and compute shader output texture depend
+                    // on window size, so if it changes they have to be updated.
+                    window_width = event.window.data1;
+                    window_height = event.window.data2;
+                    glViewport(0, 0, window_width, window_height);
+                }
+                break;
+            }
+
+            // Run user-added callbacks.
+            bool valid = true;
+            try
+            {
+                auto _ = _event_callbacks.at(event.type);
+            }
+            catch (std::out_of_range const &)
+            {
+                valid = false;
+            }
+            if (valid)
+            {
+                for (auto callback : _event_callbacks[event.type])
+                {
+                    callback(event);
+                }
+            }
+        }
+    }
+
+    /** Update the screen. */
+    void render()
+    {
+        // Update the screen.
+        SDL_GL_SwapWindow(_window);
+    }
+};
 
 
 /** Load a GLSL Shader from a file. */
@@ -39,16 +142,9 @@ Shader shader_from_file(std::string path, GLenum type)
 }
 
 
-#ifdef _WIN32
-int SDL_main(int argc, char *argv[])
-#else
-int main(int argc, char *argv[])
-#endif
+/** SDL initialization. */
+void init_SDL()
 {
-    int window_width = 640,
-        window_height = 480;
-
-    /* ===[ Initialization ]=== */
     SDL_Init(SDL_INIT_VIDEO);
 
     // Set OpenGL context version and profile (4.3 core).
@@ -56,14 +152,6 @@ int main(int argc, char *argv[])
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(
         SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-
-    // Create window and OpenGL context.
-    auto window = SDL_CreateWindow(
-        "compute",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        window_width, window_height,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-    auto context = SDL_GL_CreateContext(window);
 
     // Print the received OpenGL version.
     int major = 0,
@@ -89,23 +177,44 @@ int main(int argc, char *argv[])
         break;
     }
     std::cout << "\n";
+}
 
+
+/**
+ * OpenGL initialization.
+ * (Make sure to have an active OpenGL context before calling this!)
+ */
+void init_OpenGL()
+{
     // Init GLEW and check/print version.
     GLenum error = glewInit();
     if (error != GLEW_OK)
     {
-        std::cerr << "glewInit - " << glewGetErrorString(error) << "\n";
-        return EXIT_FAILURE;
+        throw std::runtime_error{
+            "glewInit - " + std::string{(char *)glewGetErrorString(error)}};
     }
     if (glewIsSupported("GL_VERSION_4_3") == GL_FALSE)
     {
-        std::cerr << "GLEW: OpenGL Version 4.3 not supported\n";
-        return EXIT_FAILURE;
+        throw std::runtime_error{"GLEW: OpenGL Version 4.3 not supported"};
     }
     std::cout << "GLEW Version " << glewGetString(GLEW_VERSION) << "\n";
+}
 
-    // Set OpenGL viewport.
-    glViewport(0, 0, window_width, window_height);
+
+#ifdef _WIN32
+int SDL_main(int argc, char *argv[])
+#else
+int main(int argc, char *argv[])
+#endif
+{
+    /* ===[ Initialization ]=== */
+    init_SDL();
+
+    // Create the app after SDL is initialized, but BEFORE anything that needs
+    // an OpenGL context.
+    App app{"compute", 640, 480};
+
+    init_OpenGL();
 
 
     /* ===[ Compute Shader ]=== */
@@ -115,7 +224,7 @@ int main(int argc, char *argv[])
         compute_program = new Program{
             {shader_from_file("shaders/compute.comp", GL_COMPUTE_SHADER)}};
     }
-    catch(const std::runtime_error& e)
+    catch (std::runtime_error const &e)
     {
         std::cerr << e.what() << '\n';
         return EXIT_FAILURE;
@@ -129,7 +238,7 @@ int main(int argc, char *argv[])
             shader_from_file("shaders/vertex.vert", GL_VERTEX_SHADER),
             shader_from_file("shaders/fragment.frag", GL_FRAGMENT_SHADER)}};
     }
-    catch(std::runtime_error const &e)
+    catch (std::runtime_error const &e)
     {
         std::cerr << e.what() << '\n';
         return EXIT_FAILURE;
@@ -171,28 +280,41 @@ int main(int argc, char *argv[])
     /* ===[ Compute Shader Output Texture ]=== */
     GLuint texture = 0;
     glGenTextures(1, &texture);
-    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexImage2D(
-        GL_TEXTURE_2D, 0, GL_RGBA32F, window_width, window_height, 0, GL_RGBA,
-        GL_FLOAT, nullptr);
+        GL_TEXTURE_2D, 0, GL_RGBA32F, app.window_width, app.window_height, 0,
+        GL_RGBA, GL_FLOAT, nullptr);
     glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
     glBindTexture(GL_TEXTURE_2D, 0);
+    // Since the compute shader's output texture must match the window size, we
+    // must resize it whenever the app's window size changes.
+    app.add_callback(
+        SDL_WINDOWEVENT,
+        [&texture](SDL_Event event){
+            if (event.window.event == SDL_WINDOWEVENT_RESIZED)
+            {
+                glBindTexture(GL_TEXTURE_2D, texture);
+                glTexImage2D(
+                    GL_TEXTURE_2D, 0, GL_RGBA32F, event.window.data1,
+                    event.window.data2, 0, GL_RGBA, GL_FLOAT, nullptr);
+                glBindTexture(GL_TEXTURE_2D, 0);
+            }
+        });
 
     /* ===[ Compute Shader Input Data ]=== */
     // Define the scene.
-    Material default_mat{
+    Material const default_mat{
         1.0f,
         1.0f,
         1.0f,
         15.0f,
         {1.0f, 1.0f, 1.0f}
     };
-    Sphere spheres[] = {
+    Sphere const spheres[] = {
         {
             {0.0f, 0.0f, -2.0f},
             1.0f,
@@ -204,7 +326,7 @@ int main(int argc, char *argv[])
             default_mat,
         },
     };
-    OmniLight lights[] = {
+    OmniLight const lights[] = {
         {
             {0.0f, 5.0f, -1.0f},
             {0.0f, 1.0f, 0.0f},
@@ -251,32 +373,10 @@ int main(int argc, char *argv[])
 
 
     /* ===[ Main Loop ]=== */
-    for (bool running = true; running;)
+    for (; app.running;)
     {
-        // Check events.
-        SDL_Event event{};
-        while (SDL_PollEvent(&event) && running)
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                running = false;
-                break;
-            case SDL_WINDOWEVENT:
-                if (event.window.event == SDL_WINDOWEVENT_RESIZED)
-                {
-                    // Viewport size and compute shader output texture depend
-                    // on window size, so if it changes they have to be updated.
-                    window_width = event.window.data1;
-                    window_height = event.window.data2;
-                    glViewport(0, 0, window_width, window_height);
-                    glTexImage2D(
-                        GL_TEXTURE_2D, 0, GL_RGBA32F, window_width,
-                        window_height, 0, GL_RGBA, GL_FLOAT, nullptr);
-                }
-                break;
-            }
-        }
+        // Handle user inputs.
+        app.input();
 
         /* ===[ Execute Compute Shader ]=== */
         // Use the compute shader.
@@ -310,7 +410,8 @@ int main(int argc, char *argv[])
         // Bind the light SSBO.
         glBindBuffer(GL_SHADER_STORAGE_BUFFER, light_ssbo);
         // Run the compute shader.
-        glDispatchCompute((GLuint)window_width, (GLuint)window_height, 1);
+        glDispatchCompute(
+            (GLuint)app.window_width, (GLuint)app.window_height, 1);
         // Wait for the shader to finish writing to the image.
         glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 
@@ -325,12 +426,12 @@ int main(int argc, char *argv[])
         // Use the compute output texture as the input texture.
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
-        glUniform1i(glGetUniformLocation(visual_program->id(), "tex"), 0);
+        visual_program->setUniformI("tex", 0);
         // Render the screenquad.
         glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices)/sizeof(*vertices));
 
-        // Update the screen.
-        SDL_GL_SwapWindow(window);
+        // Refresh the screen.
+        app.render();
     }
 
     // Cleanup OpenGL data.
@@ -340,9 +441,7 @@ int main(int argc, char *argv[])
     glDeleteBuffers(1, &sphere_ssbo);
     glDeleteBuffers(1, &light_ssbo);
 
-    // Cleanup SDL data.
-    SDL_GL_DeleteContext(context);
-    SDL_DestroyWindow(window);
+    // Shutdown SDL.
     SDL_Quit();
 
     return EXIT_SUCCESS;
